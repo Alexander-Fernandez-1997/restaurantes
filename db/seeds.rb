319aa0636f6d14ae2a514471dce56f3b1d89a2e9
KingAlex1997 <alex97l@hotmail.com>
2022-02-17 22:56:33 -0300

    seed ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+def rand_edad
+  rand(18...50)
+end
+
+def rand_stars
+  rand(1...5)
+end
+
+def rand_capacity
+  rand(50...300)
+end
+
+20.times do
+starter_platillos = Platillo.new(nombre: Faker::Food.dish)
+starter_platillos.save
+end
+
+10.times do
+starter_empleados = Empleado.new(nombre: Faker::Name.name_with_middle, edad: rand_edad, rol: "personal de linea")
+starter_empleados.save
+end
+
+resto = Restaurante.new(nombre: Faker::Restaurant.name, direccion: Faker::Address.street_address, estrellas: rand_stars, capacidad: rand_capacity, estilo: Faker::Restaurant.type )
+resto.empleado = Empleado.all
+resto.platillo = Platillo.all
+resto.save
+end
